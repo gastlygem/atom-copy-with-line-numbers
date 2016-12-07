7da90dfadc01c2f2c5ce6629c9d1a74ba6ea98c4
yassh <yassh32@outlook.jp>
2016-12-07 10:41:08 +0900

    first implementation

--- spec/copy-with-line-numbers-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import CopyWithLineNumbersView from '../lib/copy-with-line-numbers-view';
-
-describe('CopyWithLineNumbersView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
